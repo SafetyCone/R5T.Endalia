2349adaf0207fc4830e95494f574c6c48c935503
MinexAutomation <minex.automation@gmail.com>
2019-11-28 22:18:01 -0800

    Added endpoint identity type.

--- source/R5T.Endalia/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Endalia
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Endalia/Code/Classes/EndpointIdentity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using R5T.Guide;
+
+
+namespace R5T.Endalia
+{
+    public class EndpointIdentity : TypedGuid
+    {
+        #region Static
+
+        public static EndpointIdentity New()
+        {
+            var endpointIdentity = new EndpointIdentity();
+            return endpointIdentity;
+        }
+
+        public static EndpointIdentity From(Guid value)
+        {
+            var endpointIdentity = new EndpointIdentity(value);
+            return endpointIdentity;
+        }
+
+        #endregion
+
+
+        public EndpointIdentity(Guid value)
+            : base(value)
+        {
+        }
+
+        public EndpointIdentity()
+            : this(TypedGuid.GetNewGuid())
+        {
+        }
+    }
+}
