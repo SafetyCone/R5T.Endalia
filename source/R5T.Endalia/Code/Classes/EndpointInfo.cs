5039b6108c78f2c3ece8d6bcc4edcf7a7446a057
MinexAutomation <minex.automation@gmail.com>
2019-11-29 23:05:22 -0800

    Added info types for endpoint and endpoint-type.

---
+++ source/R5T.Endalia/Code/Classes/EndpointInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.Endalia
+{
+    public class EndpointInfo
+    {
+        public Guid Identity { get; set; }
+        public string Name { get; set; }
+        public EndpointTypeInfo TypeInfo { get; set; }
+    }
+}
---
+++ source/R5T.Endalia/Code/Classes/EndpointTypeInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Endalia
+{
+    public class EndpointTypeInfo
+    {
+        public Guid Identity { get; set; }
+        public string Name { get; set; }
+    }
+}
