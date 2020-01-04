75e443cfef559f8323fff151d5f545888ea75ef0
MinexAutomation <minex.automation@gmail.com>
2020-01-04 11:16:00 -0800

    Added static Null instance.

--- source/R5T.Endalia/Code/Classes/EndpointInfo.cs
+++ source/R5T.Endalia/Code/Classes/EndpointInfo.cs
@@ -5,6 +5,9 @@ namespace R5T.Endalia
 {
     public class EndpointInfo
     {
+        public static EndpointInfo Null = null;
+
+
         public Guid Identity { get; set; }
         public string Name { get; set; }
         public EndpointTypeInfo TypeInfo { get; set; }
